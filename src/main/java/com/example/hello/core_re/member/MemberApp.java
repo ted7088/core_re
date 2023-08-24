3c9a82e304b86fc9d2794464d541c5022b04cee1
ted3131 <ted3131@diquest.com>
2023-08-24 15:37:00 +0900

    commit

---
+++ src/main/java/com/example/hello/core_re/member/MemberApp.java
@@ -0,0 +1,16 @@
+package com.example.hello.core_re.member;
+
+public class MemberApp {
+    public static void main(String[] args) {
+
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+
+        System.out.println("find Member = " + member.getName());
+        System.out.println("new member = " + findMember.getName());
+
+    }
+}
