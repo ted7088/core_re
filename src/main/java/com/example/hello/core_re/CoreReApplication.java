1fef60b31f17cd3d24dac15f8ed570ee4c6e340a
ted3131 <ted3131@diquest.com>
2023-08-24 11:23:01 +0900

    commit

---
+++ src/main/java/com/example/hello/core_re/CoreReApplication.java
@@ -0,0 +1,13 @@
+package com.example.hello.core_re;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreReApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreReApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hello/core_re/CoreReApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hello.core_re;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreReApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
