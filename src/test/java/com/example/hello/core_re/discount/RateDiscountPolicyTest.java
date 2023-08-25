2ce0a5ddcaaa70554fc5d94c219cd158d7a7ddab
ted3131 <ted3131@diquest.com>
2023-08-25 17:25:04 +0900

    commit

--- src/test/java/com/example/hello/core_re/discount/RateDiscountPolicyTest.java
+++ src/test/java/com/example/hello/core_re/discount/RateDiscountPolicyTest.java
@@ -31,6 +31,6 @@ class RateDiscountPolicyTest {
         //when
         int discount = discountPolicy.discount(member, 10000);
         //then
-        Assertions.assertThat(discount).isEqualTo(1000);
+        Assertions.assertThat(discount).isEqualTo(0);
     }
 }
