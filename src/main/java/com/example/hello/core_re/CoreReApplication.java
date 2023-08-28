e91143cd7916b445036917a43fac6705196b2c7f
master <ted3131@diquest.com>
2023-08-28 18:24:32 +0900

    commit

--- src/main/java/com/example/hello/core_re/CoreReApplication.java
+++ src/main/java/com/example/hello/core_re/CoreReApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CoreReApplication {
-
+//test
     public static void main(String[] args) {
         SpringApplication.run(CoreReApplication.class, args);
     }
