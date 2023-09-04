beb5032da729f957a644a85a0b1621afefcf1950
master <ted3131@diquest.com>
2023-09-04 13:55:27 +0900

    commit

--- src/main/java/com/example/hello/core_re/Order/OrderServiceImpl.java
+++ src/main/java/com/example/hello/core_re/Order/OrderServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
+
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
