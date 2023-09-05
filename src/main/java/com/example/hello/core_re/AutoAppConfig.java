3d3f2e9c65cf95598c4d5da012dd75211b275b06
master <ted3131@diquest.com>
2023-09-05 10:18:32 +0900

    Revert "commit"
    This reverts commit 43605795b3b5ae1fd572e174a280f88a3627d4c8.

--- src/main/java/com/example/hello/core_re/AutoAppConfig.java
+++ src/main/java/com/example/hello/core_re/AutoAppConfig.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        basePackages = "com.example.hello.core_re.member",
-        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
